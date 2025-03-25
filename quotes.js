48fdebe2321a6f66dcbf452e49e42f825d1355e3
Grisha Mitioglo <grishamitioglo.2003@mail.ru>
2025-03-25 20:31:14 +0200

    Другой файл для цитат и импорт их  с другого файла

---
+++ quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "Life isn’t about finding yourself. Life is about creating yourself.",
+    author: "George Bernard Shaw",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser",
+  },
+  {
+    quote: "Believe you can and you’re halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+];
+export default quotes;
--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-const quotes = [
-  "Я не дерусь… Я просто помогаю людям быстро уснуть.",
-  "Я не хамлю – просто экономлю время на лишние слова.",
-  "Я не проигрываю драки… Я просто даю фору.",
-  "Девушки любят плохих парней. Я – самый плохой парень… для плохих парней.",
-  "Если я улыбаюсь, значит, что-то пошло не так… но не у меня.",
-  "Люблю спорт. Особенно тот, где надо бегать за деньгами.",
-  "Я не прихожу вовремя. Время приходит ко мне.",
-  "Когда меня спрашивают, как я решаю проблемы, я просто достаю костяшки пальцев.",
-  "Не важно, что у тебя в голове. Важно, что у тебя в руках… а у меня – ключ на 32.",
-  "Когда я захожу в лифт, лифт тоже напрягается.",
-];
+import quotes from "./quotes.js";
+
 const quoteElement = document.getElementById("quote");
 const generateBtn = document.getElementById("generate-btn");
+const quoteAuthorElement = document.getElementById("author");
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  quoteElement.textContent = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const randomQuote = quotes[randomIndex];
+  const quote = randomQuote.quote;
+  const quoteAuthor = randomQuote.author;
+  // const quote = `<em>"${randomQuote.quote}"</em><br><span id="author">${randomQuote.author}</span>`;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor;
 }
 generateBtn.addEventListener("click", generateRandomQuote);
-generateRandomQuote();
