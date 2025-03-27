a0409562f751b508d95f30fc5b35264864e6403f
Grisha Mitioglo <grishamitioglo.2003@mail.ru>
2025-03-27 15:57:40 +0200

    Добавление кнопки для избранных

--- quotes.js
+++ quotes.js
@@ -2,46 +2,56 @@ const quotes = [
   {
     quote: "Do what you can, with what you have, where you are.",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote:
       "The only limit to our realization of tomorrow is our doubts of today.",
     author: "Franklin D. Roosevelt",
+    isFavorite: false,
   },
   {
     quote:
       "Life isn’t about finding yourself. Life is about creating yourself.",
     author: "George Bernard Shaw",
+    isFavorite: false,
   },
   {
     quote:
       "Success is not final, failure is not fatal: it is the courage to continue that counts.",
     author: "Winston Churchill",
+    isFavorite: false,
   },
   {
     quote: "Opportunities don't happen. You create them.",
     author: "Chris Grosser",
+    isFavorite: false,
   },
   {
     quote: "Believe you can and you’re halfway there.",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
+    isFavorite: false,
   },
   {
     quote:
       "Do not go where the path may lead, go instead where there is no path and leave a trail.",
     author: "Ralph Waldo Emerson",
+    isFavorite: false,
   },
   {
     quote: "Happiness depends upon ourselves.",
     author: "Aristotle",
+    isFavorite: false,
   },
   {
     quote: "In the middle of every difficulty lies opportunity.",
     author: "Albert Einstein",
+    isFavorite: false,
   },
 ];
 export default quotes;
--- script.js
+++ script.js
@@ -3,14 +3,55 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const generateBtn = document.getElementById("generate-btn");
 const quoteAuthorElement = document.getElementById("author");
+const toggleFavoriteBtn = document.getElementById("make-favorite");
+const favoritesContainer = document.getElementById("favorites-container");
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  const quote = randomQuote.quote;
-  const quoteAuthor = randomQuote.author;
-  // const quote = `<em>"${randomQuote.quote}"</em><br><span id="author">${randomQuote.author}</span>`;
-  quoteElement.textContent = quote;
-  quoteAuthorElement.textContent = quoteAuthor;
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
+  quoteElement.textContent = randomQuote.quote;
+  quoteAuthorElement.textContent = randomQuote.author;
+  if (randomQuote.isFavorite) {
+    toggleFavoriteBtn.classList.add("fa");
+    toggleFavoriteBtn.classList.remove("far");
+  } else {
+    toggleFavoriteBtn.classList.remove("fa");
+    toggleFavoriteBtn.classList.add("far");
+  }
+  toggleFavoriteBtn.style.display = "inline-block";
 }
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  if (currentQuote.isFavorite) {
+    toggleFavoriteBtn.classList.add("fa");
+    toggleFavoriteBtn.classList.remove("far");
+  } else {
+    toggleFavoriteBtn.classList.remove("fa");
+    toggleFavoriteBtn.classList.add("far");
+  }
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.setAttribute("data-quote", currentQuote.quote);
+    favoriteCard.innerHTML = `
+      <p>${currentQuote.quote}</p>
+      <p class="author">${currentQuote.author}</p>
+    `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.getAttribute("data-quote") === currentQuote.quote) {
+        card.remove();
+      }
+    });
+  }
+}
+
 generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
